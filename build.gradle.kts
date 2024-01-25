f7bcf2e3f8545991a00430c6ad8016116210a37b
Chris Kocel <krzysztof.kocel@gmail.com>
2024-01-25 12:33:01 +0000

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.gitlab.arturbosch.detekt.getSupportedKotlinVersion
 
 plugins {
-    id("org.springframework.boot") version "3.1.5"
+    id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.jmailen.kotlinter") version "4.2.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.4"
